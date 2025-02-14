7a181f458686de7cf98ccb093c80a91a51d62402
Arda Köksal <109966764+KoksalArda@users.noreply.github.com>
2025-02-14 21:40:02 +0300

    koksal

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    public SceneManager level1;
+    public SceneManager mainMenu;
+    public SceneManager nextlevel;
+    void Update()
+    {
+
+        
+    }
+}
