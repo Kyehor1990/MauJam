db39903f086164d7b19276bec8d8b666a80fbfdc
Kyehor1990 <ardauludag2005@gmail.com>
2025-02-16 14:52:22 +0300

    .

--- Assets/Scripts/Buton.cs
+++ Assets/Scripts/Buton.cs
@@ -7,12 +7,17 @@ public class Buton : MonoBehaviour
 {
     [SerializeField] private GameObject kamera1;
     [SerializeField] private GameObject kamera2;
+    
+    [SerializeField] private Sprite newSprite;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            
+            spriteRenderer.sprite = newSprite;
             kamera1.SetActive(false);
             kamera2.SetActive(false);
         }
