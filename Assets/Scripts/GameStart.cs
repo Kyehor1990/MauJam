0e1a6ddeb032fe3b48c81f39f5388f3c7cd899c7
Arda Köksal <109966764+KoksalArda@users.noreply.github.com>
2025-02-16 04:57:08 +0300

    koksal

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
     public string mainMenu,nextlevel,level1;
+    public TextMeshProUGUI win;
+    bool winmi=false;
 
     public GameObject ayarlarMenusu;
     void Update()
@@ -18,21 +21,26 @@ public class GameStart : MonoBehaviour
 
 
     }
-    public void Newgame() 
+     public void winGame()
+    {
+        ayarlarMenusu.SetActive(true);
+        win.text = "Next Level";
+        winmi = true;
+    }public void Newgame() 
     {
         SceneManager.LoadScene(level1);
     }
     public void Nextlevel()
     {
-        SceneManager.LoadScene(nextlevel);
+        
+        if (winmi) {SceneManager.LoadScene(nextlevel);}
+        else { ayarlarMenusu.SetActive(false); }
+
     }
     public void mainmenu()
     {
         SceneManager.LoadScene(mainMenu);
     }
-    public void winGame()
-    {
-        gameObject.SetActive(true);
-    }
+   
 
 }
